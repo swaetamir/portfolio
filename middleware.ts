b7a76858d4fe3bdfce21716190c4d6799a790e6d
swaetamir <swaeta.mobasher@gmail.com>
2026-04-11 00:47:01 -0400

    skip auth in development for local preview

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,8 @@ import { NextRequest, NextResponse } from "next/server";
 const PASSWORD = process.env.SITE_PASSWORD ?? "changeme";
 
 export function middleware(req: NextRequest) {
+  if (process.env.NODE_ENV === "development") return NextResponse.next();
+
   const auth = req.headers.get("authorization");
 
   if (auth) {
